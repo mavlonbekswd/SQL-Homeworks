c571cacf6440f1a3d4e040168fe87b3dc2c15f41
Mavlonbek Sultanbekov <151122666+mavlonbekswd@users.noreply.github.com>
2025-08-21 12:01:57 +0100

    Update hw4.sql

--- lesson-4/homework/hw4.sql
+++ lesson-4/homework/hw4.sql
@@ -28,12 +28,12 @@ FROM Employees
 WHERE Salary >= 60000
   AND Department = 'HR';
 
-task_7
+--task_7
 SELECT EmployeeID, FirstName, LastName,
        ISNULL(Email, 'noemail@example.com') AS Email
 FROM Employees; --Agar Email bo‘sh (NULL) bo‘lsa, noemail@example.com ko‘rsatadi.
 
-task_8
+--task_8
 SELECT *
 FROM Products
 WHERE Price BETWEEN 50 AND 100;
@@ -47,3 +47,41 @@ SELECT DISTINCT Category, ProductName
 FROM Products
 ORDER BY ProductName DESC;
                                                             --MEDIUM--
+
+--task_11
+SELECT TOP 10 *
+FROM Products
+ORDER BY Price DESC;   -- eng qimmatidan arzoniga
+
+--task_12
+--COALESCE – FirstName yoki LastName’dan birinchisi (NULL bo‘lmaganini) qaytarish
+SELECT EmployeeID,
+       COALESCE(FirstName, LastName) AS PreferredName
+FROM Employees;
+-- COALESCE: birinchi NULL bo'lmagan qiymatni oladi
+
+--task-13
+--DISTINCT – Category va Price juftliklari
+SELECT DISTINCT Category, Price
+FROM Products;
+-- Ikkala ustun kombinatsiyasi bo'yicha takrorlarni olib tashlaydi
+
+--task-14
+SELECT *
+FROM Employees
+WHERE (Age BETWEEN 30 AND 40)
+   OR Department = 'Marketing';
+
+--task15
+SELECT *
+FROM Employees
+ORDER BY Salary DESC
+OFFSET 10 ROWS       -- birinchi 10 ta (1–10) tashlab ketiladi
+FETCH NEXT 10 ROWS ONLY;  -- keyingi 10 ta (11–20)
+
+--task16
+SELECT *
+FROM Products
+WHERE Price <= 1000
+  AND Stock  > 50
+ORDER BY Stock ASC;
